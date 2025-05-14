4bc7899daebcb1a398c3af4eb548d8d78a96ce10
DanielSeton <danielseton@live.nl>
2025-05-14 18:06:33 +0200

    dashboard aangemaakt en uitgewerkt

--- src/assets/constant/header.jsx
+++ src/assets/constant/header.jsx
@@ -4,22 +4,19 @@ import Signup from "../pages/signup/signup.jsx";
 function Header() {
 
     return (
-        <>
-            <header>
-                <nav className="navbar">
-                    <div className="navbar-logo">
-                        <span className="header-logo-icon"><img src="src/assets/img/placeholder-logo-icon.png"
-                                                                alt="Company logo"/></span>
-                        <p><span id="header-color-section">DJ</span>Corner</p>
-                    </div>
-                    <ul>
-                        <li><a href="/" onClick={() => console.log("Home clicked")}>Home</a></li>
-                        <li><a href="/" onClick={() => console.log("Dashboard clicked")}>Dashboard</a></li>
-                        <li><a href="/" onClick={() => console.log("Logout clicked")}>Logout</a></li>
-                    </ul>
-                </nav>
-            </header>
-        </>
+        <header>
+            <nav className="navbar">
+                <div className="navbar-logo">
+                    <span className="header-logo-icon"><img src="src/assets/img/placeholder-logo-icon.png" alt="Company logo"/></span>
+                    <p><span id="header-color-section">DJ</span>Corner</p>
+                </div>
+                <ul>
+                    <li><a href="/" onClick={() => console.log("Home clicked")}>Home</a></li>
+                    <li><a href="/" onClick={() => console.log("Dashboard clicked")}>Dashboard</a></li>
+                    <li><a href="/" onClick={() => console.log("Logout clicked")}>Logout</a></li>
+                </ul>
+            </nav>
+        </header>
     )
 }
 
